f0c7abbc43fdecac32779ca1988abcc02f8790c1
David <rozenblum3@mail.tau.ac.il>
2025-11-09 12:09:58 +0000

    Very vasic skeleton, basically just empty files

---
+++ ex1/ex1_client.py
@@ -0,0 +1,7 @@
+
+
+
+
+if __name__ == "__main__":
+    print("This is the ex1_client module.")
+    # TODO: Implement client functionality here.
---
+++ ex1/ex1_server.py
@@ -0,0 +1,5 @@
+
+
+if __name__ == "__main__":
+    print("This is the ex1_server module.")
+    # TODO: Implement server functionality here.
